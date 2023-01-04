a84a05f1423dd3b8489591447a2c2abd8bd410cc
AmbroseQiu <ambroseqiu1027@gmail.com>
2023-01-04 12:48:39 +0800

    add TestTransferAPI

---
+++ api/transfer_test.go
@@ -0,0 +1,88 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	mockdb "github.com/backendmaster/simple_bank/db/mock"
+	db "github.com/backendmaster/simple_bank/db/sqlc"
+	"github.com/backendmaster/simple_bank/util"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferAPI(t *testing.T) {
+	amount := int64(10)
+	account1 := randomAccount()
+	account2 := randomAccount()
+	account1.Currency = util.USD
+	account2.Currency = util.USD
+
+	testCase := []struct {
+		name          string
+		body          gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "ok",
+			body: gin.H{
+				"from_account_id": account1.ID,
+				"to_account_id":   account2.ID,
+				"amount":          amount,
+				"currency":        account1.Currency,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := db.TransferTxParams{
+					FromAccountID: account1.ID,
+					ToAccountID:   account2.ID,
+					Amount:        amount,
+				}
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account1.ID)).
+					Times(1).
+					Return(account1, nil)
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(account2.ID)).
+					Times(1).
+					Return(account2, nil)
+				store.EXPECT().
+					TransferTx(gomock.Any(), gomock.Eq(arg)).Times(1)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCase {
+		tc := testCase[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+
+			//build stub
+			tc.buildStubs(store)
+			//start new server and send request
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			//marshal body to json
+			data, err := json.Marshal(tc.body)
+			require.NoError(t, err)
+			url := "/transfers"
+			request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+			require.NoError(t, err)
+			server.router.ServeHTTP(recorder, request)
+			//check response
+			tc.checkResponse(t, recorder)
+		})
+	}
+
+}
