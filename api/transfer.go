ff40561530fd35776af46934c1d65d9bff84e267
AmbroseQiu <ambroseqiu1027@gmail.com>
2023-01-04 16:10:16 +0800

    rename variable

--- api/transfer.go
+++ api/transfer.go
@@ -10,40 +10,40 @@ import (
 )
 
 type transferRequest struct {
-	From_Accoount_ID int64  `json:"from_account_id" binding:"required,min=1"`
-	To_Account_ID    int64  `json:"to_account_id" binding:"required,min=1"`
-	Amount           int64  `json:"amount" binding:"required,gt=0"`
-	Currency         string `json:"currency" binding:"required,currency"`
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
 }
 
 func (server *Server) createTransfer(ctx *gin.Context) {
 	var req transferRequest
-
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errResponse(err))
 		return
 	}
 
+	if !server.validateAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+
+	if !server.validateAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
 	arg := db.TransferTxParams{
-		FromAccountID: req.From_Accoount_ID,
-		ToAccountID:   req.To_Account_ID,
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
 	}
 
-	if !server.validateAccount(ctx, req.From_Accoount_ID, req.Currency) {
-		return
-	}
-	if !server.validateAccount(ctx, req.To_Account_ID, req.Currency) {
-		return
-	}
-
-	results, err := server.store.TransferTx(ctx, arg)
+	result, err := server.store.TransferTx(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errResponse(err))
 		return
 	}
 
-	ctx.JSON(http.StatusOK, results)
+	ctx.JSON(http.StatusOK, result)
 }
 
 func (server *Server) validateAccount(ctx *gin.Context, accountID int64, currency string) bool {
