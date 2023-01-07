601c7cb15634295ea16b6a09f72187e866043eaa
AmbroseQiu <ambroseqiu1027@gmail.com>
2023-01-07 17:24:20 +0800

    add middleware and unit test fixed bug

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -23,7 +23,7 @@ func addAuthorization(
 	accessToken, err := tokenMaker.CreateToken(username, accessTokenDuration)
 	require.NoError(t, err)
 	authorizationHeader := fmt.Sprintf("%s %s", authorizationType, accessToken)
-	request.Header.Set(authorizationType, authorizationHeader)
+	request.Header.Set(authorizationHeaderKey, authorizationHeader)
 }
 
 func TestMiddleWare(t *testing.T) {
@@ -42,41 +42,41 @@ func TestMiddleWare(t *testing.T) {
 				require.Equal(t, http.StatusOK, recorder.Code)
 			},
 		},
-		// {
-		// 	name: "no authorization",
-		// 	addAuth: func(request *http.Request, tokenMaker token.Maker) {
-		// 	},
-		// 	checkResponse: func(recorder *httptest.ResponseRecorder) {
-		// 		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-		// 	},
-		// },
-		// {
-		// 	name: "unsupported authorizatio type",
-		// 	addAuth: func(request *http.Request, tokenMaker token.Maker) {
-		// 		addAuthorization(t, request, tokenMaker, "unsupported", "user", time.Minute)
-		// 	},
-		// 	checkResponse: func(recorder *httptest.ResponseRecorder) {
-		// 		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-		// 	},
-		// },
-		// {
-		// 	name: "invalid authorizatio type",
-		// 	addAuth: func(request *http.Request, tokenMaker token.Maker) {
-		// 		addAuthorization(t, request, tokenMaker, "", "user", time.Minute)
-		// 	},
-		// 	checkResponse: func(recorder *httptest.ResponseRecorder) {
-		// 		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-		// 	},
-		// },
-		// {
-		// 	name: "expired token",
-		// 	addAuth: func(request *http.Request, tokenMaker token.Maker) {
-		// 		addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", -time.Minute)
-		// 	},
-		// 	checkResponse: func(recorder *httptest.ResponseRecorder) {
-		// 		require.Equal(t, http.StatusUnauthorized, recorder.Code)
-		// 	},
-		// },
+		{
+			name: "no authorization",
+			addAuth: func(request *http.Request, tokenMaker token.Maker) {
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name: "unsupported authorizatio type",
+			addAuth: func(request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, "unsupported", "user", time.Minute)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name: "invalid authorizatio type",
+			addAuth: func(request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, "", "user", time.Minute)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
+		{
+			name: "expired token",
+			addAuth: func(request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, authorizationTypeBearer, "user", -time.Minute)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
 	}
 
 	for i := range testCase {
