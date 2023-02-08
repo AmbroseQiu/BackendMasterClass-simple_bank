3efa2939e8cfa0cd4fa23b2cb3538e4f87b5ef5a
AmbroseQiu <ambroseqiu1027@gmail.com>
2023-02-08 23:39:52 +0800

    add metadat extraction

---
+++ gapi/metadata.go
@@ -0,0 +1,42 @@
+package gapi
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+const (
+	grpcGatewayUserAgentHeader = "grpcgateway-user-agent"
+	userAgentHeader            = "user-agent"
+	xForwardForHeader          = "x-forwarded-for"
+)
+
+type Metadata struct {
+	UserAgent string
+	ClientIP  string
+}
+
+func (server *Server) extractMetadata(ctx context.Context) *Metadata {
+	mtdt := &Metadata{}
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		log.Printf("md :%v", md)
+		if userAgent := md.Get(grpcGatewayUserAgentHeader); len(userAgent) > 0 {
+			mtdt.UserAgent = userAgent[0]
+		}
+		if userAgent := md.Get(userAgentHeader); len(userAgent) > 0 {
+			mtdt.UserAgent = userAgent[0]
+		}
+		if clientIP := md.Get(xForwardForHeader); len(clientIP) > 0 {
+			mtdt.ClientIP = clientIP[0]
+		}
+	}
+	if p, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = p.Addr.String()
+	}
+
+	return mtdt
+}
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -36,12 +36,13 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		return nil, status.Errorf(codes.Internal, "create refresh token failed %s", err)
 	}
 
+	mtdt := server.extractMetadata(ctx)
 	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
 		ID:           refreshPayload.ID,
 		Username:     refreshPayload.Username,
 		RefreshToken: refreshToken,
-		UserAgent:    "",
-		ClientIp:     "",
+		UserAgent:    mtdt.UserAgent,
+		ClientIp:     mtdt.ClientIP,
 		IsBlocked:    false,
 		ExpiresAt:    refreshPayload.ExpiredAt,
 	})
